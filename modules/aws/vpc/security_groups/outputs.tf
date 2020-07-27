02f6db26fff90246b90043a67f278132bd024e7f
Fabricio <fabripautasso@gmail.com>
2020-07-27 11:47:39 -0300

    Adding a new output for security groups

--- modules/aws/vpc/security_groups/outputs.tf
+++ modules/aws/vpc/security_groups/outputs.tf
@@ -5,3 +5,7 @@ output "groups_from_everywhere" {
 output "groups_to_everywhere" {
   value = aws_security_group.allow_to_everywhere
 }
+
+output "groups_all_to_everywhere" {
+  value = aws_security_group.allow_all_to_everywhere
+}
