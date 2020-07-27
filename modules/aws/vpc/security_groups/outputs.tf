ce30f4d0c609d4b29f2e98be99272472ced4e385
Fabricio <fabripautasso@gmail.com>
2020-07-27 16:07:51 -0300

    Updating security groups

--- modules/aws/vpc/security_groups/outputs.tf
+++ modules/aws/vpc/security_groups/outputs.tf
@@ -2,12 +2,8 @@ output "groups_from_everywhere" {
   value = aws_security_group.allow_from_everywhere
 }
 
-output "groups_to_everywhere" {
-  value = aws_security_group.allow_to_everywhere
-}
-
-output "groups_all_to_everywhere" {
-  value = aws_security_group.allow_all_to_everywhere
+output "groups_allow_all_to_intranet" {
+  value = aws_security_group.allow_all_to_intranet.id
 }
 
 output "groups_all_to_everywhere_id" {
