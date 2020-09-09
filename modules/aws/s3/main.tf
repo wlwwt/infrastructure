f9a1fffd738f4a767e748548a535df1b6278ce2f
epicatto <epicatto@hotmail.com>
2020-09-09 14:48:40 -0300

    Add S3 bucket module
    - Enable SSE by default.

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "main" {
     enabled = var.versioning_enabled
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = var.kms_master_key_arn
+        sse_algorithm     = var.sse_algorithm
+      }
+    }
+  }
+
   tags = {
     Project = var.app_name
     Stage   = var.environment
--- modules/aws/s3/variables.tf
+++ modules/aws/s3/variables.tf
@@ -17,3 +17,12 @@ variable "versioning_enabled" {
   default     = false
   description = "A state of versioning (keeping multiple variants of an object in the same bucket)"
 }
+variable "sse_algorithm" {
+  type        = string
+  description = "The server-side encryption algorithm to use. Valid values are AES256 and aws:kms."
+}
+variable "kms_master_key_arn" {
+  type        = string
+  default     = ""
+  description = "The AWS KMS master key ID used for the SSE-KMS encryption. Used only if algorithm is aws:kms"
+}
